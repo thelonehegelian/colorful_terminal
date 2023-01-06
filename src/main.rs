1328fda88452fc5bc7779198ec26ba3a9f041f1a
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-06 20:52:16 +0000

    Update text

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use ansi_term::Colour;
 
 fn main() {
     println!(
-        "This is in red: {}{}",
-        Colour::Red.paint("a red string"),
-        Colour::Yellow.italic().paint(" and it is in Italic"),
+        "{} {} {}",
+        Colour::Red.paint("A red string"),
+        Colour::Yellow.italic().paint("and it is in Italic"),
+        Colour::Green.paint(", and we have a green string"),
     );
 }
